c71f9ef69eb18836c041f1a3c8159546766a69a0
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-05-29 09:18:35 +0100

    Update Props.js

--- src/Flexisaf internship/Props.js
+++ src/Flexisaf internship/Props.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Property(props) {
     return(
-        <div className="events">
+        <div className="event">
         <h2>My name is {props.fullname} and I was rendered using React props</h2>
         </div>
     )
