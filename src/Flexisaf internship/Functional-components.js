e55144087b6e26fd48cceb35bedb39eaad18bd7d
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-05-29 09:16:35 +0100

    Update Functional-components.js

--- src/Flexisaf internship/Functional-components.js
+++ src/Flexisaf internship/Functional-components.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function Functionalcompo() {
     return(
+        <div className="funcompo">
         <h1>This element was rendered using a functional component</h1>
+        </div>
     )
 }
 export default Functionalcompo;
